74fdfb1f2bb555b9bdd0e9884e0981131b3e6be3
Lucas Lacquaniti <lucasla@MacBook-Pro-de-Lisa.local>
2019-08-21 16:03:06 -0300

    Finished first insertion of tweets

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express(); // crea una instancia de una aplicación de express
 const nunjucks = require("nunjucks");
 const https = require("https");
 const http = require("http");
+const routes = require('./routes/index.js');
 
-var myLogger = function(req, res, next) {
+app.use(express.static('public'))
+
+app.use('/', routes);
+
+/*var myLogger = function(req, res, next) {
   console.log("LOGGED");
   next();
-};
+};*/
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+const people = [{ name: "Full" }, { name: "Stacker" }, { name: "Son" }];
+
 
 nunjucks.configure("views", {
   autoescape: true,
@@ -28,10 +34,21 @@ https.createServer(app).listen(443);
 app.engine("html", nunjucks.render);
 app.set("view engine", "html");
 
-app.use(myLogger);
+app.get("/", function(req, res){
 
-app.get("/", function(req, res) {
-  res.render("index", { title: "TooOooOOnto a las tres", people: people});
+    res.sendFile("/stylesheets/styles.css", function(err){
+
+        if(err){
+            next(err)
+        }else{
+            console.log("Sent");
+        }
+    })
+})
+
+
+/*app.get("/", function(req, res) {
+  res.render("index", { title: "TooOooOOnto a las tres", people: people });
 });
 
 app.post("/", function() {
@@ -39,7 +56,8 @@ app.post("/", function() {
 });
 
 app.get("/special*", function(req, res) {
-    res.render("index", { people: title, people: people });
-});
+  res.render("index", { people: title, people: people });
+});*/
 
 app.listen(8080);
+
